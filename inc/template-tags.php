4d353ce2a183e0f82fab5df2cc8a9643d0d3e631
blemmie <julievanopdenbosch@gmail.com>
2021-02-15 21:44:23 +0100

    adapt to more minimal look again, comment out links on post meta data for date & author

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -28,7 +28,8 @@ if ( ! function_exists( 'blemmie_posted_on' ) ) :
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
 			esc_html_x( 'Posted on %s', 'post date', 'blemmie' ),
-			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+			/* ORIGINAL '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'*/
+			$time_string
 		);
 
 		echo '<span class="posted-on">' . $posted_on . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
@@ -44,7 +45,8 @@ if ( ! function_exists( 'blemmie_posted_by' ) ) :
 		$byline = sprintf(
 			/* translators: %s: post author. */
 			esc_html_x( 'by %s', 'post author', 'blemmie' ),
-			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+			/* ORIGINAL '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'*/
+			'<span class="author vcard">' . esc_html( get_the_author() ) . '</span>'
 		);
 
 		echo '<span class="byline"> ' . $byline . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
